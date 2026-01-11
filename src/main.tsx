908df33e12385b92cc48aef928e6762ff9c16249
MrPoy <gregory.poupaux@hotmail.fr>
2026-01-11 13:20:19 +0100

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
 import ErrorBoundary from './components/ErrorBoundary.tsx'
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/mentions-legales" element={<MentionsLegales />} />
           <Route path="/confidentialite" element={<Confidentialite />} />
         </Routes>
+        <Analytics />
       </BrowserRouter>
     </ErrorBoundary>
   </StrictMode>,
